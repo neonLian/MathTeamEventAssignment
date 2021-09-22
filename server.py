a29b049fde3585a689858039484b5106cfd4de1f
neonLian <vortextornado42@gmail.com>
2021-09-21 17:08:43 -1000

    server changes

--- server.py
+++ server.py
@@ -4,31 +4,31 @@ import pandas as pd
 from solve import findBestAssignment
 
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+#from werkzeug import secure_filename
 app = Flask(__name__)
 
 @app.route('/upload')
-def upload_file():
+def upload_html():
     return render_template('app.html')
-	
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         #f.save(secure_filename(f.filename))
-        output = process_data(file)
+        output = process_data(f)
 
         if output['success']:
-            return 'file uploaded successfully'
+            return 'Best team: ' + str(output['best_score']) + '\n' + str(output['df'])
         else:
             return 'Failed: ' + output['reason']
-    
+
 def process_data(file):
-    
-    output = {'success': False, 'reason': 'Unknown', 'best_score': 0, 'best_team': []}
-    
+
+    output = {'success': False, 'reason': 'Unknown', 'best_score': 0, 'best_team': [], 'df': None}
+
     df = pd.read_csv(file)
-    
+
     # Identify the important columns
     name_col = None
     event_cols = [None,None,None,None,None,None]
@@ -42,20 +42,26 @@ def process_data(file):
         output['success'] = False
         output['reason'] = 'Could not find necessary column'
         return output
-    
+
     # Split dataframe into names and scores
     scores_df = df[event_cols]
+<<<<<<< Updated upstream
 	scores_df.fillna(0, inplace=True)
     scores_arr = scores_df.to_numpy().astype(int)
     
+=======
+    scores_arr = scores_df.to_numpy()
+
+>>>>>>> Stashed changes
     names = df[name_col]
-    
+
     # Send information to solver program
     solverResult = findBestAssignment(scores_arr)
     output['success'] = True
     output['best_score'] = solverResult['best_score']
     output['best_team'] = solverResult['best_team']
+    output['df'] = df
     return output
-		
+
 if __name__ == '__main__':
    app.run(debug = True)
